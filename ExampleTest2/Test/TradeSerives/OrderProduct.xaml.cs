1cf3543683536b4dbab84a9125e7c832099e2958
Georgi <georgi2012bg@mail.bg>
2022-02-17 18:57:25 +0200

    Added sample exam solution

---
+++ ExampleTest2/Test/TradeSerives/OrderProduct.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TradeSerives
+{
+    /// <summary>
+    /// Interaction logic for OrderProduct.xaml
+    /// </summary>
+    public partial class OrderProduct : UserControl
+    {
+        public class OrderEventArgs : EventArgs
+        {
+            private string prodID;
+            private int qty;
+            public OrderEventArgs(string id,int _qty)
+            {
+                qty = _qty;
+                prodID = id;
+            }
+            public string ProductID
+            {
+                get
+                {
+                    return prodID;
+                }
+            }
+            public int Quantity
+            {
+                get
+                {
+                    return qty;
+                }
+            }
+        }
+        public delegate void OrderEventHandler(object sender, OrderEventArgs e);
+        public event OrderEventHandler OnOrder;
+        public OrderProduct()
+        {
+            InitializeComponent();
+        }
+
+        public ComboBox ComboBoxProduct
+        {
+            get
+            {
+                return CmbBoxProduct;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBoxQty.Text = "0";
+        }
+
+        private void BtnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            int qty;
+            if (!int.TryParse(TxtBoxQty.Text,out qty)){
+                MessageBox.Show("Invalid Quantity number!");
+                return;
+            }
+            if(CmbBoxProduct.SelectedItem == null)
+            {
+                MessageBox.Show("No item selected!");
+                return;
+            }
+            OnOrder?.Invoke(this,new OrderEventArgs(((Product)CmbBoxProduct.SelectedItem).ID,qty));
+        }
+    }
+}
---
+++ ExampleTest2/Test/TradeSerives/Product.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeSerives
+{
+    [Serializable]//dont forget!
+    public class Product
+    {
+
+        public readonly string ID;
+        private static uint NUMBER = 0;
+        private Category productCategory;
+        private int qty;
+        private int reorderLevel;
+
+        //ctors
+        public Product(Category cat, int _qty, int reorLvl,string _ID="")
+        {
+            //with 4 leading zeros
+            if (string.IsNullOrEmpty(_ID))
+            {
+                _ID=string.Format("P{0:D4}", NUMBER++);
+            }
+            ID = _ID;
+            productCategory = cat;
+            qty = _qty;
+            reorderLevel = reorLvl;
+        }
+        public Product(Product other)
+            : this(other.ProductCategory, other.Qty, other.ReorderLevel,other.ID){}
+        public Product() : this(Category.INVALID, 0, 0, string.Format("P{0:D4}", NUMBER++)) { } //for serialization
+        public Category ProductCategory
+        {
+            get
+            {
+                return productCategory;
+            }
+            set
+            {
+                productCategory = value;
+            }
+        }
+        public int Qty
+        {
+            get
+            {
+                return qty;
+            }
+            set
+            {
+                qty = value >= 0 ? value : 0;
+            }
+        }
+        public int ReorderLevel
+        {
+            get
+            {
+                return reorderLevel;
+            }
+            set
+            {
+                reorderLevel = value >= 0 ? value : 0;
+            }
+        }
+       
+        public override string ToString()
+        {
+            return string.Format($"PID:{ID}, Cat:{productCategory.ToString()}, Qty:{qty} ,Reorder at:{reorderLevel}");
+        }
+
+    }
+}
