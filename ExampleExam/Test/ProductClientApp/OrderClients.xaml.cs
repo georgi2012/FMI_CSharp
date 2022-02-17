f9affb651e481fbd6e59a94570c1f50cd6506883
Georgi <georgi2012bg@mail.bg>
2022-02-17 18:58:57 +0200

    Added sample exam solution

---
+++ ExampleExam/Test/ProductClientApp/OrderClients.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeSerives;
+using static TradeSerives.OrderProduct;
+
+namespace ProductClientApp
+{
+    
+    public partial class OrderClients : Window
+    {
+        private OrderProduct orderProduct;//user control
+        private ServiceReference1.OrderWServiceClient client;
+    
+        public OrderClients()
+        {
+            InitializeComponent();
+            orderProduct = UCOrdPrd;
+            Random random = new Random();
+            this.Title = string.Format("Order client {0}", random.Next(1, 1000));
+            // 
+            client = new ServiceReference1.OrderWServiceClient();
+            Dispatcher.Invoke(new Action(() =>
+            {
+                Product[] pr = client.Retrieve();
+                orderProduct.ComboBoxProduct.ItemsSource = pr;
+            }));
+            orderProduct.OnOrder += OnUpdateRequest;
+        }
+
+        private void OnUpdateRequest(object sender, OrderEventArgs e)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                client.Update(this.Title,e.ProductID,e.Quantity);
+            }));
+        }
+    }
+}
---
+++ ExampleExam/Test/TradeSOAPService/TradeProducts.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+using TradeSerives;
+
+namespace TradeSOAPService
+{
+  
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single,
+        ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class TradeProducts : IOrderWService
+    {
+        private Dictionary<string, Product> products;
+        //static object locker = new object();
+        public TradeProducts()
+        {
+            const int startQty = 12;
+            Random rng = new Random();
+            int prodCount = rng.Next(6, 11);
+            int maxCategories = Enum.GetNames(typeof(Category)).Length;
+            products = new Dictionary<string, Product>(prodCount);
+            for (int i = 0; i < prodCount; i++)
+            {
+                //making random product
+                Category cat = (Category)rng.Next(1, maxCategories - 1);//0 invalid
+                int lvl = rng.Next(6, 12);
+                Product product = new Product(cat, startQty, lvl);
+                //
+                products[product.ID] = product;
+            }
+            //
+            Thread thread = new Thread(CheckQty);
+            thread.Start();
+        }
+
+        private void CheckQty()
+        {
+            Random rng = new Random();
+            while (true)
+            {
+                foreach (var prod in products.Values)
+                {
+                    if (prod.Qty <= prod.ReorderLevel)
+                    {
+                        prod.Qty += rng.Next(prod.ReorderLevel, prod.ReorderLevel + 11);
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        public Product[] Retrieve()
+        {
+            lock (products) //thread-safety first!
+            {
+                Product[] prodArr = new Product[products.Count];
+                int ind = 0;
+                foreach (var prod in products.Values)
+                {
+                    prodArr[ind++] = new Product(prod);
+                }
+                return prodArr;
+            }
+        }
+
+        public void Update(string sender, string productID, int qty)
+        {
+            lock (products) {
+                const string fileName = "C:\\Test\\reorder.txt";
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+                using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    writer.WriteLine(string.Format($"{sender}:{products[productID]},Wanted:{qty};"));
+                }
+
+            }
+        }
+    }
+}
